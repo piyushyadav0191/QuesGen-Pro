74fc710a2d179b8210e6415e5125846f0b5df9f1
Piyush Yadav <piyushyadav0191@gmail.com>
2023-09-28 09:54:27 +0530

    some UI fixing

--- src/app/leaderboard/components/Leaderboard.tsx
+++ src/app/leaderboard/components/Leaderboard.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import React from "react";
 import { formatFullDateAndTime } from "../../../../utils/formatTime";
 
@@ -26,35 +17,43 @@ type Props = {
 };
 
 const Leaderboard = ({ topUsers }: Props) => {
-  return (
-    <Table className="bg-white">
-      <TableCaption className="text-black">
-        Top Scorer of the Month
-      </TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[150px] text-black">User id</TableHead>
-          <TableHead className="text-black">Name</TableHead>
-          <TableHead className="text-black">Date & Time</TableHead>
-          <TableHead className="text-black">Accuracy</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {topUsers.map((users) => (
-          <TableRow key={users.userId}>
-            <TableCell className="font-medium text-black">
-              {users.userId}
-            </TableCell>
-            <TableCell className="text-black">{users._max.userName}</TableCell>
+  const getColorClass = (index: number) => {
+    // Define an array of background colors for the boxes
+    const backgroundColors = [
+      "bg-blue-200",
+      "bg-green-200",
+      "bg-yellow-200",
+      "bg-pink-200",
+      "bg-purple-200",
+    ];
 
-            <TableCell className="text-black">
+    // Use modulo to cycle through the background colors
+    return backgroundColors[index % backgroundColors.length];
+  };
+
+  return (
+    <div className="w-full">
+      <h1 className="text-black text-2xl font-bold mb-4">Leaderboard</h1>
+      <h2 className="text-black text-lg font-semibold mb-2">
+        Top Scorer of the Month
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {topUsers.map((users, index) => (
+          <div
+            key={users.userId}
+            className={`bg-white p-4 shadow-md ${getColorClass(index)}`}
+          >
+            <div className="flex justify-between">
+              <p className="text-black">{users._max.userName}</p>
+              <p className="text-black">{users._max.accuracy}%</p>
+            </div>
+            <p className="text-black mt-2">
               {formatFullDateAndTime(users._max.createdAt)}
-            </TableCell>
-            <TableCell className="text-black">{users._max.accuracy}</TableCell>
-          </TableRow>
+            </p>
+          </div>
         ))}
-      </TableBody>
-    </Table>
+      </div>
+    </div>
   );
 };
 
