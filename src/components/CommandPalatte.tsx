f812a40b5f7382d471667c3cffdc5e421ab135e9
Piyush Yadav <piyushyadav0191@gmail.com>
2023-09-28 09:39:15 +0530

    some UI fixing

--- src/components/CommandPalatte.tsx
+++ src/components/CommandPalatte.tsx
@@ -19,11 +19,10 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-  CommandSeparator,
-  CommandShortcut,
 } from "@/components/ui/command";
 import Link from "next/link";
 import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
 
 export function CommandPalatte() {
   const { setTheme } = useTheme();
@@ -41,51 +40,62 @@ export function CommandPalatte() {
     return () => document.removeEventListener("keydown", down);
   }, []);
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2 } },
+  };
+
   return (
     <>
       <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded  px-1.5 font-mono text-xl font-medium ">
         <span className="text-xl">⌘</span>J
       </kbd>
-      <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Search a page..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <LayoutDashboard className="mr-2 h-4 w-4" />
-              <Link href={"/dashboard"}>Dashboard</Link>
-            </CommandItem>
-            <CommandItem>
-              <FileQuestion className="mr-2 h-4 w-4" />
-              <Link href={"/mcq"}>MCQ / Open Ended</Link>
-            </CommandItem>
-            <CommandItem>
-              <User2 className="mr-2 h-4 w-4" />
-              <Link href={"/profile"}>Profile</Link>
-            </CommandItem>
-            <CommandItem>
-              <History className="mr-2 h-4 w-4" />
-              <Link href={"/history"}>History</Link>
-            </CommandItem>
-            <CommandItem>
-              <CircuitBoard className="mr-2 h-4 w-4" />
-              <Link href={"/leaderboard"}>Leaderboard</Link>
-            </CommandItem>
-            <CommandItem>
-              <Smile className="mr-2 h-4 w-4" />
-              <Link href={"/relax-space"}>Relax space</Link>
-            </CommandItem>
-            <CommandItem>
-              <Sun className="mr-2 h-4 w-4" />
-              <span onClick={() => setTheme("light")}>Light mode</span>
-            </CommandItem>
-            <CommandItem>
-              <Moon className="mr-2 h-4 w-4" />
-              <span onClick={() => setTheme("dark")}>Dark mode</span>
-            </CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </CommandDialog>
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder="Search a page..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>
+                <LayoutDashboard className="mr-2 h-4 w-4" />
+                <Link href={"/dashboard"}>Dashboard</Link>
+              </CommandItem>
+              <CommandItem>
+                <FileQuestion className="mr-2 h-4 w-4" />
+                <Link href={"/mcq"}>MCQ / Open Ended</Link>
+              </CommandItem>
+              <CommandItem>
+                <User2 className="mr-2 h-4 w-4" />
+                <Link href={"/profile"}>Profile</Link>
+              </CommandItem>
+              <CommandItem>
+                <History className="mr-2 h-4 w-4" />
+                <Link href={"/history"}>History</Link>
+              </CommandItem>
+              <CommandItem>
+                <CircuitBoard className="mr-2 h-4 w-4" />
+                <Link href={"/leaderboard"}>Leaderboard</Link>
+              </CommandItem>
+              <CommandItem>
+                <Smile className="mr-2 h-4 w-4" />
+                <Link href={"/relax-space"}>Relax space</Link>
+              </CommandItem>
+              <CommandItem>
+                <Sun className="mr-2 h-4 w-4" />
+                <span onClick={() => setTheme("light")}>Light mode</span>
+              </CommandItem>
+              <CommandItem>
+                <Moon className="mr-2 h-4 w-4" />
+                <span onClick={() => setTheme("dark")}>Dark mode</span>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
+      </motion.div>
     </>
   );
 }
