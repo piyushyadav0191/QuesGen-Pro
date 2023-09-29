5477fc51908e76398184ad9d49cb1f73f2df282f
Piyush Yadav <piyushyadav0191@gmail.com>
2023-09-29 15:50:06 +0530

    command palatte fixed

--- src/components/CommandPalatte.tsx
+++ src/components/CommandPalatte.tsx
@@ -44,7 +44,6 @@ export function CommandPalatte() {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { duration: 2 } },
   };
-
   return (
     <>
       <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded  px-1.5 font-mono text-xl font-medium ">
@@ -60,38 +59,54 @@ export function CommandPalatte() {
           <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
             <CommandGroup heading="Suggestions">
-              <CommandItem>
-                <LayoutDashboard className="mr-2 h-4 w-4" />
-                <Link href={"/dashboard"}>Dashboard</Link>
-              </CommandItem>
-              <CommandItem>
-                <FileQuestion className="mr-2 h-4 w-4" />
-                <Link href={"/mcq"}>MCQ / Open Ended</Link>
-              </CommandItem>
-              <CommandItem>
-                <User2 className="mr-2 h-4 w-4" />
-                <Link href={"/profile"}>Profile</Link>
-              </CommandItem>
-              <CommandItem>
-                <History className="mr-2 h-4 w-4" />
-                <Link href={"/history"}>History</Link>
-              </CommandItem>
-              <CommandItem>
-                <CircuitBoard className="mr-2 h-4 w-4" />
-                <Link href={"/leaderboard"}>Leaderboard</Link>
-              </CommandItem>
-              <CommandItem>
-                <Smile className="mr-2 h-4 w-4" />
-                <Link href={"/relax-space"}>Relax space</Link>
-              </CommandItem>
-              <CommandItem>
-                <Sun className="mr-2 h-4 w-4" />
-                <span onClick={() => setTheme("light")}>Light mode</span>
-              </CommandItem>
-              <CommandItem>
-                <Moon className="mr-2 h-4 w-4" />
-                <span onClick={() => setTheme("dark")}>Dark mode</span>
-              </CommandItem>
+              <Link href={"/dashboard"}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <LayoutDashboard className="mr-2 h-4 w-4" />
+                  Dashboard
+                </CommandItem>
+              </Link>
+              <Link href={"/mcq"}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <FileQuestion className="mr-2 h-4 w-4" />
+                  MCQ / Open Ended
+                </CommandItem>
+              </Link>
+              <Link href={"/profile"}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <User2 className="mr-2 h-4 w-4" />
+                  Profile
+                </CommandItem>
+              </Link>
+              <Link href={"/history"}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <History className="mr-2 h-4 w-4" />
+                  History
+                </CommandItem>
+              </Link>
+              <Link href={"/leaderboard"}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <CircuitBoard className="mr-2 h-4 w-4" />
+                  Leaderboard
+                </CommandItem>
+              </Link>
+              <Link href={"/relax-space"}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <Smile className="mr-2 h-4 w-4" />
+                  Relax space
+                </CommandItem>
+              </Link>
+              <button onClick={() => setTheme("light")}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <Sun className="mr-2 h-4 w-4" />
+                  Light mode
+                </CommandItem>
+              </button>
+              <button onClick={() => setTheme("dark")}>
+                <CommandItem onSelect={() => setOpen(false)}>
+                  <Moon className="mr-2 h-4 w-4" />
+                  Dark mode
+                </CommandItem>
+              </button>
             </CommandGroup>
           </CommandList>
         </CommandDialog>
