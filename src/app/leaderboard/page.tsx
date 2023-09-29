96f712cdecd0f168dcf9336f3f8bc28070f6b2cb
Piyush Yadav <piyushyadav0191@gmail.com>
2023-09-29 15:53:15 +0530

    leaderboard signed out fixed

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Leaderboard from "./components/Leaderboard";
 import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
 const page = async (props: Props) => {
+  const session = await getAuthSession();
   // only one highest accuracy record of each user
+  if (!session?.user) {
+    return redirect("/");
+  }
 
   const topUsers = await prisma.accuracyRecord.groupBy({
     by: ["userId"],
