a0777035ed2ac9c5aac407c47a779108c6945035
Piyush Yadav <piyushyadav0191@gmail.com>
2024-06-25 20:29:13 +0530

    chore: commented openai code

--- src/app/api/learning/route.ts
+++ src/app/api/learning/route.ts
@@ -2,11 +2,6 @@ import { prisma } from "@/lib/db";
 import { getAuthSession } from "@/lib/nextauth";
 import { careerAdviceSchema } from "@/schemas/form/mcq";
 import { NextResponse } from "next/server";
-import OpenAI from 'openai';
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 export const dynamic = 'force-dynamic';
 
@@ -35,33 +30,33 @@ export const POST = async (req: Request, res: Response) => {
     const body = await req.json();
     const { topic, experienced } = careerAdviceSchema.parse(body);
 
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
-          content: `You are a helpful AI that is able to generate career advice as the answer to that question and do not answer anything other than career advice, store the answer and question in a JSON array. You are to generate an answer about ${topic} for ${experienced}`,
-        },
-      ],
-    });
+    // const response = await openai.chat.completions.create({
+    //   model: 'gpt-3.5-turbo',
+    //   messages: [
+    //     {
+    //       role: 'system',
+    //       content: `You are a helpful AI that is able to generate career advice as the answer to that question and do not answer anything other than career advice, store the answer and question in a JSON array. You are to generate an answer about ${topic} for ${experienced}`,
+    //     },
+    //   ],
+    // });
     
-    await prisma.careerAdvice.create({
-      data: {
-        name: session?.user?.name as string,
-        createdAt: new Date(),
-        careerAdvice: JSON.stringify(response.choices[0].message),
-        userId: session?.user?.id as string,
-      },
-    })
+    // await prisma.careerAdvice.create({
+    //   data: {
+    //     name: session?.user?.name as string,
+    //     createdAt: new Date(),
+    //     careerAdvice: JSON.stringify(response.choices[0].message),
+    //     userId: session?.user?.id as string,
+    //   },
+    // })
 
-    await prisma.user.update({
-      where: {
-        id: session?.user?.id,
-      },
-      data: {
-        hasGeneratedAdvice: true,
-      },
-    });
+    // await prisma.user.update({
+    //   where: {
+    //     id: session?.user?.id,
+    //   },
+    //   data: {
+    //     hasGeneratedAdvice: true,
+    //   },
+    // });
 
     return NextResponse.json("success", {
       status: 200,
