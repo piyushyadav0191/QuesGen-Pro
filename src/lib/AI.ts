d7a3fb651ba09b0ca03d85fa25c59eb85cce368d
Piyush Yadav <piyushyadav0191@gmail.com>
2024-06-25 20:24:59 +0530

    chore: updating env variable from client side visible to server side

--- src/lib/AI.ts
+++ src/lib/AI.ts
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY as string;
+const apiKey = process.env.GEMINI_API_KEY as string;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
